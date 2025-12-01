58ded211835c067ab8f4ab6825f60599b08c9e48
Menegol <gsmenegol749@gmail.com>
2025-12-01 19:42:25 -0300

    Novo base URL

--- frontEnd/src/services/api.ts
+++ frontEnd/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = import.meta.env.MODE === "development"
     ? 'http://localhost:5000/api'
-    : 'YSVdominio/api';
+    : 'https://ysv-saude-mental.onrender.com/api';
 
 
 const api = axios.create({
